cee37ae69c700d3dd14dad319d2cf71caa723997
Radik Zainullin <radik.zainullin@gmail.com>
2023-12-15 16:34:09 +0300

    добавлен calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -15,20 +15,35 @@
 })(); */
 
 // второе решение
-const calculator = new (function () {
-  let a;
-  let b;
-  this.read = function (x, y) {
-    a = x;
-    b = y;
-  };
-  this.sum = function () {
-    return a + b;
-  };
-  this.mul = function () {
-    return a * b;
-  };
-})();
+// const calculator = new (function () {
+//   let a;
+//   let b;
+//   this.read = function (x, y) {
+//     a = x;
+//     b = y;
+//   };
+//   this.sum = function () {
+//     return a + b;
+//   };
+//   this.mul = function () {
+//     return a * b;
+//   };
+// })();
+
+let calculator = {
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+};
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
